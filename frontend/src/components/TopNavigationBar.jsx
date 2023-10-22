94cb6f91411ce6186480d42195f79e718c5e5430
Nirakone Phromkharanourak <119075500+Nirakone@users.noreply.github.com>
2023-10-21 21:28:17 -0400

    Add files via upload

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
-import '../styles/TopNavigationBar.scss'
+import '../styles/TopNavigationBar.scss';
+import TopicList from 'components/TopicList';
 
-const TopNavigation = () => {
+const TopNavigation = (props) => {
+
   return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">PhotoLabs</span>
+      <span className="top-nav-bar__logo" onClick={() => props.getPhotosOfTopic('logo')}>PhotoLabs</span>
+      <TopicList
+        topics={props.topics}
+        toggleNotification={props.toggleNotification}
+        getPhotosOfTopic={props.getPhotosOfTopic}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default TopNavigation;
--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,17 +2,13 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: "1",
-  slug: "topic-1",
-  label: "Nature",
-};
+const TopicListItem = (props) => {
+  const { getPhotosOfTopic } = props;
 
-const TopicListItem = () => {
   return (
     <div className="topic-list__item">
-      {/* Insert React */}
-    </div>
+      <span onClick={() => getPhotosOfTopic(props.topic.id)}>{props.topic.title}</span>
+    </div >
   );
 };
 
